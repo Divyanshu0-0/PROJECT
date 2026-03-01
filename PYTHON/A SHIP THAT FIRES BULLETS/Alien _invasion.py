7ee6e4765b9974d894b560506bdb72741a2ce6e1
Divyanshu <dvnsh76@gmail.com>
2026-03-01 19:04:36 +0530

    commit

--- PYTHON/A SHIP THAT FIRES BULLETS/Alien _invasion.py
+++ PYTHON/A SHIP THAT FIRES BULLETS/Alien _invasion.py
@@ -18,7 +18,58 @@ class AlienInvasion:
 
             # Make the most recently draw screen visible.
             pygame.display.flip()
+
 if __name__ == '__main__':
     # Make a Game instance and run the game.
-    alien = AlienInvasion()
-    alien.run_game()
+    ai = alien = AlienInvasion()
+    ai.run_game()
+
+def __init__(self):
+    """ Initialize the game ,and create game resources"""
+    pygame.init()
+    self.clock = pygame.time.Clock()
+
+def run_game(self):
+    """Start the main loop for the game."""
+    while True:
+
+        pygame.display.flip()
+        pygame.clock.tick(90)
+def __init__(self):
+
+    pygame.display.set_caption("Alien Invasion")
+
+    # Set the background color.
+    self.bg_colour = (230, 230, 230)
+
+def run_game(self):
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.bg_color)
+
+        # Make the most recently draw screen visible.
+        pygame.display.flip()
+        self.clock.tick(90)
+
+# -------------------------------------------------------------
+
+import pygame
+from settings import Settings
+
+class AlienInvasion:
+    """ Overall class to manage game assets and behavior."""
+
+def __init__(self):
+    """ Initialize class to manage game resources. """
+    pygame.init()
+    self.clock = pygame.time.Clock()
+    self.settings = Settings()
+
+    self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+            # Redraw the screen
---
+++ PYTHON/A SHIP THAT FIRES BULLETS/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """ A class to store all settings for Alien Invasions. """
+    def __init__(self):
+        """ Initialization the game's settings. """
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
