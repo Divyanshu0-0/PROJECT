7b16f681c35f6c5fb13d8a7d9f6ad17f7d7d2b26
Divyanshu <dvnsh76@gmail.com>
2026-03-02 17:47:33 +0530

    commit

--- PYTHON/A SHIP THAT FIRES BULLETS/Alien _invasion.py
+++ PYTHON/A SHIP THAT FIRES BULLETS/Alien _invasion.py
@@ -72,4 +72,27 @@ def __init__(self):
     pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
-            # Redraw the screen
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        # make the most recently draw screen visible.
+        pygame.display.flip()
+        self.Clock.tick(90)
+
+# --------------------------------------------------------------------
+
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """ Overall class t manages game assets and behivior. """
+    def __init__(self):
+        pygame.display.set_caption("Alien Invasion ")
+
+        self.ship = Ship(self)
+    def run_game(self):
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.setting.bg_color)
+        self.ship.blitme()
+        # Make the most recently draw screen visible.
+        pygame.display.flip()
+        self.clock.tick(60)
